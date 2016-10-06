5c413d1c841fb29c73221c64b28e4e2e4cd7a994
stevijo <stevijo.mayer@googlemail.com>
2016-10-06 15:23:23 +0200

    bug fix
    Signed-off-by: stevijo <stevijo.mayer@googlemail.com>

--- src/controllers/LoginController.cpp
+++ src/controllers/LoginController.cpp
@@ -3,15 +3,23 @@
 //
 
 #include "controllers/LoginController.h"
+#include "jwt/Token.h"
 #include "header/XLiveTubeAuth.h"
 #include "header/XGithubAuth.h"
 
 void LoginController::login(const Rest::Request& request, Http::ResponseWriter response) {
 
     auto headers = request.headers();
+    auto token = headers.get<XLiveTubeAuth>();
     auto github = headers.get<XGithubAuth>();
 
-    Github cache(github->getToken());
+    Token jwtToken(token->getToken());
+    auto res = jwtToken.decodeAndValid();
+    if(!res) {
+        response.send(Http::Code::Bad_Request);
+        return;
+    }
+    Github cache(github->getToken(),jwtToken.getGrant("sub"));
     cache.updateCache();
 
     H::addCorsHeaders(response);
